e36fcb139284012c67c028c86b38377a97bd6eea
Woodenspoon <wmwarzecha@gmail.com>
2025-02-17 11:06:30 +0100

    update logo, remove map zooming

--- src/about/map-section/map.js
+++ src/about/map-section/map.js
@@ -22,9 +22,7 @@ const mapSection = (() => {
         {x: 52.97, y: 44.89, name: "Extra Facility", description: "I'm extra facility"},
         {x: 80.49, y: 80.18, name: "Giga Facility", description: "I'm giga facility"}
     ];
-
-
-
+   
     mapPoints.forEach((point) => {
         const pointDiv = document.createElement("div");
         pointDiv.classList.add("map-point");
@@ -38,41 +36,50 @@ const mapSection = (() => {
         zoomDiv.appendChild(pointDiv);
     });
 
-    let scale = 1;
-    let startDistance = 0;
 
+    // let scale = 1;
+    // let startDistance = 0;
+    // zoomDiv.classList.add("zoom-div");
     // Handle touchpad pinch & mouse scroll zoom
-    zoomDiv.addEventListener("wheel", function (event) {
-        if (event.ctrlKey || event.deltaY !== 0) { 
-            event.preventDefault();
-            scale += event.deltaY * -0.01;
-            scale = Math.min(Math.max(1, scale), 3);
-            zoomDiv.classList.add("zoom-div");
-            zoomDiv.style.transform = `scale(${scale})`;
-        }
-    });
+    // zoomDiv.style.setProperty('--translate-x', `0%`);
+    // zoomDiv.style.setProperty('--scale', `1`);
+    // mapContainer.addEventListener("wheel", function (e) {
+    //     if (e.ctrlKey || e.deltaY !== 0) { 
+    //         e.preventDefault();
+    //         scale += e.deltaY * -0.01;
+    //         scale = Math.min(Math.max(1, scale), 3);
+    //         zoomDiv.style.setProperty('--scale', `${scale}`);
+    //         if(scale > 1 && scale < 3 && e.deltaY>0)zoomDiv.style.setProperty('--translate-x', `${Math.min(50, Math.max(-50, parseInt(zoomDiv.style.getPropertyValue('--translate-x')) - 10*(e.clientX - mapContainer.offsetLeft - mapContainer.clientWidth / 2) / (scale*mapContainer.clientWidth)))}%`);
+    //         if(scale >= 1 && scale < 3 && e.delyaY<=0)zoomDiv.style.setProperty('--translate-x', `${100*(1-scale)/2}%`);
+    //         // zoomDiv.style.transform = `translateX(-${2* (e.clientX - mapContainer.offsetLeft - mapContainer.clientWidth / 2) / mapContainer.clientWidth}%)`;
+    //         // zoomDiv.style.transform = `scale(${scale}) translateX(${10*(e.clientX - mapContainer.offsetLeft - mapContainer.clientWidth / 2) / (mapContainer.clientWidth)}%)`;
+    //         // console.log(parseInt(zoomDiv.style.getPropertyValue('--translate-x')) + 10*(e.clientX - mapContainer.offsetLeft - mapContainer.clientWidth / 2) / (mapContainer.clientWidth));
+    //         // console.log(((e.clientX - mapContainer.offsetLeft - mapContainer.clientWidth / 2) / mapContainer.clientWidth) * mapContainer.scrollWidth, mapContainer.scrollWidth)
+    //         console.log(100*(1-scale)/2);
+    //     }
+    // });
 
     // Handle mobile pinch-to-zoom
-    zoomDiv.addEventListener("touchstart", (e) => {
-        if (e.touches.length === 2) { 
-            startDistance = getDistance(e.touches[0], e.touches[1]);
-        }
-    });
+    // zoomDiv.addEventListener("touchstart", (e) => {
+    //     if (e.touches.length === 2) { 
+    //         startDistance = getDistance(e.touches[0], e.touches[1]);
+    //     }
+    // });
 
-    zoomDiv.addEventListener("touchmove", (e) => {
-        if (e.touches.length === 2) {
-            e.preventDefault();
-            let newDistance = getDistance(e.touches[0], e.touches[1]);
-            let zoomFactor = newDistance / startDistance;
-            scale = Math.min(Math.max(1, scale * zoomFactor), 3);
-            zoomDiv.style.transform = `scale(${scale})`;
-        }
-    });
+    // zoomDiv.addEventListener("touchmove", (e) => {
+    //     if (e.touches.length === 2) {
+    //         e.preventDefault();
+    //         let newDistance = getDistance(e.touches[0], e.touches[1]);
+    //         let zoomFactor = newDistance / startDistance;
+    //         scale = Math.min(Math.max(1, scale * zoomFactor), 3);
+    //         zoomDiv.style.transform = `scale(${scale})`;
+    //     }
+    // });
 
-    // Function to get touch distance
-    function getDistance(touch1, touch2) {
-        return Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
-    };
+    // // Function to get touch distance
+    // function getDistance(touch1, touch2) {
+    //     return Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
+    // };
 
    
     mapContainer.appendChild(mapModalContainer.container);
